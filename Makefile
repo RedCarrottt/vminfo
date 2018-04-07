528dc11b82288f0b85f8b90ceb6bf9ede02e3ec9
Gyeonghwan Hong <RedCarrottt@gmail.com>
2018-04-07 07:37:27 +0000

    Modified for any kernel versions

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 vminfo : main.cpp ProcessGroup.cpp Process.cpp
-	g++ -o vminfo main.cpp ProcessGroup.cpp Process.cpp
+	g++ -g -o vminfo main.cpp ProcessGroup.cpp Process.cpp
 clean :
 	rm vminfo 
--- Process.cpp
+++ Process.cpp
@@ -105,171 +105,73 @@ void Process::getPssFromSmaps() {
   int total_rss = 0;
   int total_private_clean = 0;
   int total_private_dirty = 0;
+
   while(fgets(buffer, 1024, fp)) {
     const int LINE_PER_OBJECT = 19;
     int line_idx = line_num % LINE_PER_OBJECT;
+    int expected_header = 1;
 
-    switch(line_idx) {
-      case 0:		// Header : Address, Permissions, Offset, Time, Size, Object Name
-        {
-          int tok_idx = 0;
-          char* tok = strtok(buffer, " ");
-          char* read_object_name = NULL;
-          while(tok != NULL) {
-            switch(tok_idx) {
-              case 5:
-                // Read object name
-                read_object_name = new char[strlen(tok) + 1];
-                memcpy(read_object_name, tok, strlen(tok) + 1);
-                break;
-            }
-            tok = strtok(NULL, " ");
-            tok_idx++;
-          }
-          // EXCEPTION If there is not object name for the object, set default object name
-          int length = strlen(read_object_name) + 1;
-          if(read_object_name == NULL || length <= 2) {
-            const char DEFAULT_NAME[] = "anonymous";
-            read_object_name = new char[strlen(DEFAULT_NAME) + 1];
-            memcpy(read_object_name, DEFAULT_NAME, strlen(DEFAULT_NAME) + 1);
-          }
-          // HERE Read object name
-          // not use for present
-          for(int j=0; j<length; j++) {
-            if(read_object_name[j] == '\n') read_object_name[j] = ' ';
+    if(expected_header == 1) {
+      // Header : Address, Permissions, Offset, Time, Size, Object Name
+      int tok_idx = 0;
+      char* tok = strtok(buffer, " ");
+      char* read_object_name = NULL;
+      while(tok != NULL) {
+        switch(tok_idx) {
+          case 5:
+            // Read object name
+            read_object_name = new char[strlen(tok) + 1];
+            memcpy(read_object_name, tok, strlen(tok) + 1);
+            break;
+        }
+        tok = strtok(NULL, " ");
+        tok_idx++;
+      }
+      // EXCEPTION If there is not object name for the object, set default object name
+      if(read_object_name == NULL || strlen(read_object_name) <= 1) {
+        const char DEFAULT_NAME[] = "anonymous";
+        read_object_name = new char[strlen(DEFAULT_NAME) + 1];
+        memcpy(read_object_name, DEFAULT_NAME, strlen(DEFAULT_NAME) + 1);
+      }
+      // HERE Read object name
+      // not use for present
+      for(int j=0; j<strlen(read_object_name); j++) {
+        if(read_object_name[j] == '\n') read_object_name[j] = ' ';
+      }
+      expected_header = 0;
+      continue;
+    } else {
+      char* tok = strtok(buffer, " ");
+      int tok_idx = 0;
+      while(tok != NULL) {
+        if(tok_idx == 0) {
+          char* title = tok;
+          if(strncmp(title, "Rss", 3) == 0) {
+            // Rss (KB)
+            tok = strtok(NULL, " "); tok_idx++;
+            total_rss += atoi(tok);
+            break;
+          } else if(strncmp(title, "Pss", 3) == 0) {
+            // Pss (KB)
+            tok = strtok(NULL, " "); tok_idx++;
+            total_pss += atoi(tok);
+            break;
+          } else if(strncmp(title, "Private_Clean", 13) == 0) {
+            // Private_Clean (KB)
+            tok = strtok(NULL, " "); tok_idx++;
+            total_private_clean += atoi(tok);
+            break;
+          } else if(strncmp(title, "Private_Dirty", 13) == 0) {
+            // Private_Dirty (KB)
+            tok = strtok(NULL, " "); tok_idx++;
+            total_private_dirty += atoi(tok);
+            break;
           }
         }
-        break;
-      case 1:		// Size
-        break;
-      case 2:		// Rss (KB)
-        {
-          int tok_idx = 0;
-          char* tok = strtok(buffer, " ");
-          int read_object_rss = -1;
-          while(tok != NULL) {
-            switch(tok_idx) {
-              case 1:
-                read_object_rss = atoi(tok);
-                // HERE read object rss
-                // add its rss to total rss
-                total_rss += read_object_rss;
-                // TODO set object's rss
-                break;
-            }
-            tok = strtok(NULL, " ");
-            tok_idx++;
-          }
-          // EXCEPTION If there is not object name for the object, set as an error occured
-          if(read_object_rss == -1) {
-            this->mInitFailed = 1;
-            return;
-          }
-        }
-        break;
-      case 3:		// Pss (KB)
-        {
-          int tok_idx = 0;
-          char* tok = strtok(buffer, " ");
-          int read_object_pss = -1;
-          while(tok != NULL) {
-            switch(tok_idx) {
-              case 1:
-                read_object_pss = atoi(tok);
-                // HERE read object pss
-                // add its pss to total pss
-                total_pss += read_object_pss;
-                // TODO set object's pss
-                break;
-            }
-            tok = strtok(NULL, " ");
-            tok_idx++;
-          }
-          // EXCEPTION If there is not object name for the object, set as an error occured
-          if(read_object_pss == -1) {
-            this->mInitFailed = 1;
-            return;
-          }
-        }
-        break;
-      case 4:		// Shared Clean
-        break;
-      case 5:		// Shared Dirty
-        break;
-      case 6:		// Private Clean
-        {
-          int tok_idx = 0;
-          char* tok = strtok(buffer, " ");
-          int read_object_private_clean = -1;
-          while(tok != NULL) {
-            switch(tok_idx) {
-              case 1:
-                read_object_private_clean = atoi(tok);
-                // HERE read object private clean
-                // add its priavet clean to total private clean
-                total_private_clean += read_object_private_clean;
-                // TODO set object's private clean
-                break;
-            }
-            tok = strtok(NULL, " ");
-            tok_idx++;
-          }
-          // EXCEPTION If there is not object name for the object, set as an error occured
-          if(read_object_private_clean == -1) {
-            this->mInitFailed = 1;
-            return;
-          }
-        }
-        break;
-      case 7:		// Private Dirty
-        {
-          int tok_idx = 0;
-          char* tok = strtok(buffer, " ");
-          int read_object_private_dirty = -1;
-          while(tok != NULL) {
-            switch(tok_idx) {
-              case 1:
-                read_object_private_dirty = atoi(tok);
-                // HERE read object private dirty
-                // add its priavet clean to total private dirty
-                total_private_dirty += read_object_private_dirty;
-                // TODO set object's private dirty
-                break;
-            }
-            tok = strtok(NULL, " ");
-            tok_idx++;
-          }
-          // EXCEPTION If there is not object name for the object, set as an error occured
-          if(read_object_private_dirty == -1) {
-            this->mInitFailed = 1;
-            return;
-          }
-        }
-        break;
-      case 8:		// Referenced
-        break;
-      case 9:		// Anonymous
-        break;
-      case 10:	// AnonHughPages
-        break;
-      case 11:  // Shared Huge TLB
-        break;
-      case 12:  // Private Huge TLB
-        break;
-      case 13:	// Swap
-        break;
-      case 14:  // Swap PSS
-        break;
-      case 15:	// KernelPageSize
-        break;
-      case 16:	// MMUPagesSize
-        break;
-      case 17:	// Locked
-        break;
-      case 18:  // VM Flags
-        break;
+        tok = strtok(NULL, " ");
+        tok_idx++;
+      }
     }
-    line_num++;
   }
   // set rss & pss of the pid to process snapshot
   this->mPss = total_pss;
